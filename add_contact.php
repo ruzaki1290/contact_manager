d8c4e764d7f36163f2ae0fc7438b79d0dcf24154
Rus <rus.zakirov@triosstudent.com>
2024-09-23 22:02:43 -0400

    Added more code to add_contact page and created error page

--- add_contact.php
+++ add_contact.php
@@ -11,6 +11,7 @@
    if ($first_name == null || $last_name == null || 
    $email_address == null || $phone_number == null)
    {
+      // if validation fails, redirect to error page
       $_SESSION["add_error"] = "Invalid contact data! Check all of 
       the fields and try again";
 
@@ -19,11 +20,31 @@
        die();
    } else {
       require_once('database.php');
-
+      
+      // if validation success, we will create an SQL statement in the database using the form
       // Add the contact to the database(fields inside the database; has to match the names to the letter and in the same order)
       $query = 'INSERT INTO contacts
          (firstName, lastName, emailAddress, phone)
          VALUES
          (:firstName, :lastName, :emailAddress, :phone)';
+      
+      $statement = $db->prepare($query);
+      $statement->bindValue(':firstName', $first_name);
+      $statement->bindValue(':lastName', $last_name);
+      $statement->bindValue(':emailAddress', $email_address);
+      $statement->bindValue(':phone', $phone_number);
+
+      // processes to the database
+      $statement->execute();
+      // close prepared statement
+      $statement->closeCursor();
    }
+
+      // create session with a full name
+      $_SESSION["fullName"] = $first_name . " " . $last_name;
+
+      // redirect to confirmation page(hidden page, that the user cannot see; the page serves processing needs only)
+      $url = "confirmation.php";
+      header("Location: " . $url);
+       die();
 ?>
